e36c5f2ec5356892ef21b3f510f0ff203eb2e57c
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2026-06-22 15:03:23 +0700

    Update : Perubahan di DB

--- fio_db.sql
+++ fio_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 09, 2026 at 09:39 AM
+-- Generation Time: Jun 22, 2026 at 09:38 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,28 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projects`
+-- Table structure for table `notifications`
 --
 
-CREATE TABLE `projects` (
+CREATE TABLE `notifications` (
   `id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL,
-  `project_name` varchar(150) NOT NULL,
-  `description` text DEFAULT NULL,
-  `created_by` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `project_updates`
+-- Table structure for table `progresses`
 --
 
-CREATE TABLE `project_updates` (
+CREATE TABLE `progresses` (
   `id` int(11) NOT NULL,
   `project_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `creator_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
@@ -54,25 +54,40 @@ CREATE TABLE `project_updates` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `roles`
+-- Table structure for table `progress_assets`
 --
 
-CREATE TABLE `roles` (
+CREATE TABLE `progress_assets` (
   `id` int(11) NOT NULL,
-  `role_name` varchar(50) NOT NULL
+  `progress_id` int(11) NOT NULL,
+  `file_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `roles`
+-- Table structure for table `projects`
 --
 
-INSERT INTO `roles` (`id`, `role_name`) VALUES
-(4, 'Actor'),
-(3, 'Director'),
-(6, 'Editor'),
-(1, 'Owner'),
-(5, 'Videographer'),
-(2, 'Writer');
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `project_name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_members`
+--
+
+CREATE TABLE `project_members` (
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `role` enum('Owner','Director','Writer','Editor','Videographer','Actor') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -82,53 +97,24 @@ INSERT INTO `roles` (`id`, `role_name`) VALUES
 
 CREATE TABLE `schedules` (
   `id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL,
-  `title` varchar(150) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `creator_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
-  `start_datetime` datetime NOT NULL,
-  `end_datetime` datetime NOT NULL,
-  `created_by` int(11) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `schedule_members`
+-- Table structure for table `schedule_roles`
 --
 
-CREATE TABLE `schedule_members` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `schedule_roles` (
   `schedule_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teams`
---
-
-CREATE TABLE `teams` (
-  `id` int(11) NOT NULL,
-  `team_name` varchar(100) NOT NULL,
-  `description` text DEFAULT NULL,
-  `created_by` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `team_members`
---
-
-CREATE TABLE `team_members` (
-  `id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  `joined_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `role` enum('Owner','Director','Writer','Editor','Videographer','Actor') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -139,11 +125,11 @@ CREATE TABLE `team_members` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `profile_picture` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -151,70 +137,84 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `progresses`
+--
+ALTER TABLE `progresses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `creator_id` (`creator_id`);
+
+--
+-- Indexes for table `progress_assets`
+--
+ALTER TABLE `progress_assets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `progress_id` (`progress_id`);
+
+--
 -- Indexes for table `projects`
 --
 ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `team_id` (`team_id`),
-  ADD KEY `created_by` (`created_by`);
+  ADD KEY `owner_id` (`owner_id`);
 
 --
--- Indexes for table `project_updates`
+-- Indexes for table `project_members`
 --
-ALTER TABLE `project_updates`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `project_id` (`project_id`),
+ALTER TABLE `project_members`
+  ADD PRIMARY KEY (`project_id`,`user_id`),
   ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `role_name` (`role_name`);
 
 --
 -- Indexes for table `schedules`
 --
 ALTER TABLE `schedules`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `team_id` (`team_id`),
-  ADD KEY `created_by` (`created_by`);
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `creator_id` (`creator_id`);
 
 --
--- Indexes for table `schedule_members`
+-- Indexes for table `schedule_roles`
 --
-ALTER TABLE `schedule_members`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `schedule_id` (`schedule_id`,`user_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `teams`
---
-ALTER TABLE `teams`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `created_by` (`created_by`);
-
---
--- Indexes for table `team_members`
---
-ALTER TABLE `team_members`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `team_id` (`team_id`,`user_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `role_id` (`role_id`);
+ALTER TABLE `schedule_roles`
+  ADD PRIMARY KEY (`schedule_id`,`role`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `progresses`
+--
+ALTER TABLE `progresses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `progress_assets`
+--
+ALTER TABLE `progress_assets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `projects`
@@ -223,39 +223,9 @@ ALTER TABLE `projects`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `project_updates`
---
-ALTER TABLE `project_updates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `schedule_members`
---
-ALTER TABLE `schedule_members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `teams`
---
-ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `team_members`
---
-ALTER TABLE `team_members`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -269,46 +239,49 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `progresses`
+--
+ALTER TABLE `progresses`
+  ADD CONSTRAINT `progresses_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `progresses_ibfk_2` FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `progress_assets`
+--
+ALTER TABLE `progress_assets`
+  ADD CONSTRAINT `progress_assets_ibfk_1` FOREIGN KEY (`progress_id`) REFERENCES `progresses` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `projects`
 --
 ALTER TABLE `projects`
-  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `project_updates`
+-- Constraints for table `project_members`
 --
-ALTER TABLE `project_updates`
-  ADD CONSTRAINT `project_updates_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `project_updates_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+ALTER TABLE `project_members`
+  ADD CONSTRAINT `project_members_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `project_members_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `schedules`
 --
 ALTER TABLE `schedules`
-  ADD CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `schedules_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `schedules_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `schedules_ibfk_2` FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `schedule_members`
+-- Constraints for table `schedule_roles`
 --
-ALTER TABLE `schedule_members`
-  ADD CONSTRAINT `schedule_members_ibfk_1` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `schedule_members_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `teams`
---
-ALTER TABLE `teams`
-  ADD CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `team_members`
---
-ALTER TABLE `team_members`
-  ADD CONSTRAINT `team_members_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `team_members_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `team_members_ibfk_3` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+ALTER TABLE `schedule_roles`
+  ADD CONSTRAINT `schedule_roles_ibfk_1` FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
